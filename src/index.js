c87153072e81daa624b8672baa359daaa8ca0ca0
Potatodori <jshj5181@naver.com>
2023-03-05 02:41:58 +0900

    라이브러리 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import dayjs from "dayjs";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const app = express();
+
+app.use(cors());
+app.use(helmet()); //미들웨어
+
+const today = new Date();
+const todayToDayjs = dayjs(today).format("YYYY-MM-DD");
+console.log({ today, todayToDayjs });
+
+const password = "1234";
+const hashedPassword = bcrypt.hashSync(password, 10);
+console.log({ hashedPassword });
+
+const token = jwt.sign("1234", "aefrgbqetwabqwedcwevbrweqbwQ");
+console.log({ token });
 
 app.get("/", (req, res) => {
   res.send("Express");
